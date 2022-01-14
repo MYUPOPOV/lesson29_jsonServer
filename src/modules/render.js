f1c3c9f7b57a81bdc90f439aa58e24147debf4ad
MYuPopov <beemax@bk.ru>
2022-01-14 17:17:52 +0300

    First commit

---
+++ src/modules/render.js
@@ -0,0 +1,12 @@
+export const render = (users) => {
+	users.forEach((user) => {
+		console.log(user);
+	});
+	// userService.users = [
+	// 	{
+	// 		id: 0,
+	// 	},
+	// ];
+
+	// userService.logger();
+};
